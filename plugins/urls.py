f836b6a1935bffe678f39d7bb80c8652fda19ef0
illing <sebastian.illing@met.fu-berlin.de>
2013-11-14 14:48:24 +0100

    refacoring: added "solr" App

--- plugins/urls.py
+++ plugins/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns('plugins.views',
     url(r'^$', 'home', name='home'),
     url(r'^browse-files/$', 'dirlist', name='dirlist'),
-    url(r'^solr-search/$', 'solr_search', name='solr_search'),
+#    url(r'^solr-search/$', 'solr_search', name='solr_search'),
     url(r'^(?P<plugin_name>\w+)/detail/$', 'detail', name='detail'),
     url(r'^(?P<plugin_name>\w+)/setup/$', 'setup', name='setup'),
     url(r'^(?P<plugin_name>\w+)/(?P<row_id>\d+)/setup/$', 'setup', name='setup'),   
--- plugins/views.py
+++ plugins/views.py
@@ -133,32 +133,7 @@ def dirlist(request):
         r.append('</ul>')
     return HttpResponse(''.join(r))  
 
-@login_required()
-def solr_search(request):
-    args = dict(request.GET)#self.request.arguments.copy()
-    latest = True#bool(request.GET.get('latest', [False]))
-    try:
-        facets = request.GET['facet']
-    except KeyError:
-        facets = False
-    if 'start' in args: args['start'] = int(request.GET['start']) 
-    if 'rows' in args: args['rows'] = int(request.GET['rows'])
-    
-    metadata = None
-    #return HttpResponse(json.dumps(dict(args)))
-    if facets:
-        args.pop('facet')
-        if facets == '*':
-            #means select all, 
-            facets = None
-        results = SolrFindFiles.facets(facets=facets, **args)
-    else:
-        #return HttpResponse(json.dumps(dict(hallo='was')))
-        results = SolrFindFiles.search( _retrieve_metadata = True, **args)
-        metadata = results.next()
-        results = list(results)
-      
-    return HttpResponse(json.dumps(dict(data=results, metadata=metadata)), content_type="application/json")
+
 
 
 

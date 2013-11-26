ff193fa71194ea8d91c5f4fdce6ad59b080757ca
kunst <oliver.kunst@met.fu-berlin.de>
2013-11-26 10:29:41 +0100

    Global URL for preview

--- django_evlauation/settings/base.py
+++ django_evlauation/settings/base.py
@@ -172,18 +172,15 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Example: "http://media.example.com/static/"
 STATIC_URL = '/static/'
 
-# Absolute path to the preview directory, should match
-# the entry in config.py of the evaluation system
-PREVIEW_ROOT = os.path.join(STATIC_ROOT, 'preview')
-
 # URL for preview files
-PREVIEW_URL = '/static/preview/'
+PREVIEW_URL = STATIC_URL + 'preview/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    ('preview', '/miklip/integration/evaluation_system/database/preview'),
 )
 
 # If you set this to False, Django will make some optimizations so as not

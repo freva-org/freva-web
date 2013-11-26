ed39a29699614fa6163272b9a30ddebb32842a3a
kunst <oliver.kunst@met.fu-berlin.de>
2013-11-26 17:57:16 +0100

    Added an restart option for tail which deletes the offset file

--- history/utils.py
+++ history/utils.py
@@ -6,7 +6,7 @@ from django_evlauation import settings
 
 
 
-def pygtailwrapper(id):
+def pygtailwrapper(id, restart = False):
     """
     This function return the pygtail result for the slurm file
     specified in the history
@@ -25,7 +25,8 @@ def pygtailwrapper(id):
     offset_file_name = os.path.join(settings.TAIL_TMP_DIR, file_name)
     offset_file_name = offset_file_name + '.offset'
     
-    
+    if restart and os.path.isfile(offset_file_name):
+        os.remove(offset_file_name)
     
     
     return Pygtail(full_file_name, offset_file=offset_file_name)    

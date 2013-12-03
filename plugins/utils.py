a2c499b32d3249abb055e0ae68edb8f0a36966e4
kunst <oliver.kunst@met.fu-berlin.de>
2013-12-03 12:23:50 +0100

    No automatic SS key lookup

--- plugins/utils.py
+++ plugins/utils.py
@@ -1,4 +1,5 @@
 import evaluation_system.api.plugin_manager as pm
+from django.views.decorators.debug import sensitive_variables
 from django.http import Http404
 
 import paramiko
@@ -10,7 +11,8 @@ def get_plugin_or_404(plugin_name,user=None):
     except:
         raise Http404
     
-    
+
+@sensitive_variables('password')    
 def ssh_call(username, password, command, hostname='127.0.0.1'):
     """
     executes a command under the given user on a remote machine.
@@ -27,7 +29,10 @@ def ssh_call(username, password, command, hostname='127.0.0.1'):
 
     # except remote key anyways
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname=hostname, username=username, password=password)
+    ssh.connect(hostname=hostname,
+                username=username,
+                password=password,
+                look_for_keys=False)
 
     (stdin, stdout, stderr) = ssh.exec_command(command=command)
 

8f598c49f8da5813b61c4b7e213bd234406e9650
illing <sebastian.illing@met.fu-berlin.de>
2013-12-16 18:06:47 +0100

    added "murcss" flatpage and imgages

--- plugins/urls.py
+++ plugins/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns('plugins.views',
     url(r'^$', 'home', name='home'),
+    url(r'^about/$', 'listDocu', name='about'),
     url(r'^browse-files/$', 'dirlist', name='dirlist'),
 #    url(r'^solr-search/$', 'solr_search', name='solr_search'),
     url(r'^(?P<plugin_name>\w+)/detail/$', 'detail', name='detail'),

cff5ea0575c1b2f63bd0e0abf2096312db20536c
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2014-01-08 17:54:22 +0100

    added preview_tree template tag for displaying results

---
+++ history/templatetags/resulttags.py
@@ -0,0 +1,64 @@
+from django import template
+from django.utils.safestring import mark_safe
+from django.utils.encoding import force_text
+from django.utils.html import conditional_escape
+from django.template.loader import render_to_string
+
+from django_evlauation import settings
+
+import logging
+
+register = template.Library()
+
+
+@register.filter(is_safe=True, needs_autoescape=True)
+def preview_tree(value, autoescape=None):
+    """
+    Template filter to display a FileDict defined in history.utils.FileDict
+    
+    :param value: instance of FileDict (compressed_copy)    
+    """
+    if autoescape:
+        escaper = conditional_escape
+    else:
+        escaper = lambda x: x
+       
+    def _helperDict(dict_, depth=0):
+        '''
+        Helper function for the nested display process 
+        Output is a file tree (unordered list) and preview images using fancybox2
+        
+        :param dict_: compressed_copy of a FileDict
+        :param depth: tree depth
+        '''
+        output = []
+        for key,value in sorted(dict_.items()):
+            subdict = ''
+            subdict_item = None
+            if isinstance(dict_[key], dict):
+                subdict_item = dict_[key]
+
+            #if child is a dictionary use recursion
+            if subdict_item:
+                subdict = _helperDict(subdict_item,depth+1)
+                if isinstance(subdict_item.values()[0],dict):
+                    subdict = '\n<ul class="jqueryFileTree">\n%s\n</ul>\n' % (subdict,)
+                else:
+                    subdict = '\n<div class="row" >%s</div>\n' % (subdict,)
+
+            #next lines define css-sytles of listitems
+            if depth > 1:
+                visible = 'style="display:none;"'
+            else:
+                visible = ''
+            if depth > 0:
+                folder_image = 'directory collapsed'
+            else:
+                folder_image = 'directory expanded'
+
+            if not subdict_item:
+                output.append(render_to_string('history/templatetags/preview-img.html', {'imgname':key, 'preview':value, 'PREVIEW_URL':settings.PREVIEW_URL}))
+            else:
+                output.append('<li class="%s" %s><a href="#">%s</a>%s</li>' % (folder_image, visible, escaper(force_text(key)), subdict))
+        return '\n'.join(output)
+    return mark_safe(_helperDict(value))

38b338f4fa4b8a86fa862045237d1e8d463e4e37
Oliver Kunst <u290038@wwwdev-miklip.dkrz.de>
2014-01-24 15:25:43 +0100

    added redirect for favicon

--- django_evaluation/urls.py
+++ django_evaluation/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls import include, patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.views.generic import RedirectView
 admin.autodiscover()
 
 
@@ -24,6 +25,7 @@ urlpatterns = patterns('',
     url(r'^bad/$', bad),
     url(r'', include('base.urls', namespace='base')),
     
+    url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico'))
 )
 
 ## In DEBUG mode, serve media files through Django.

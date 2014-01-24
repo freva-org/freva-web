cb62b27e1ef6b6dfa6ba801a87d5853939a46051
kunst <oliver.kunst@met.fu-berlin.de>
2014-01-24 15:04:08 +0100

    added favicon to webpage

--- base/urls.py
+++ base/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('base.views',
     url(r'^logout', 'logout', name='logout'),
     url(r'^wiki', 'wiki', name='wiki'),
     url(r'^contact', 'contact', name='contact'),
+    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.ico'}),
+
 )

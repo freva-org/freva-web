6f8a0c52da1099b65bd6cd5746b0eda95fcc3fb4
Oliver Kunst <u290038@miklip11.dkrz.de>
2014-03-04 18:27:58 +0100

    Removed buggy **args from solr_search call

--- django_evaluation/settings/base.py
+++ django_evaluation/settings/base.py
@@ -33,7 +33,7 @@ def get_env_setting(setting):
 # The server for LDAP configuration
 AUTH_LDAP_SERVER_URI = "ldaps://dsp1.dkrz.de, ldaps://dsp2.dkrz.de"
 
-FACETS = SolrFindFiles.facets(facets=None,**args)
+FACETS = SolrFindFiles.facets(facets=None)
 
 # The directory with SSL certificates
 CA_CERT_DIR = '/etc/openldap/cacerts/'

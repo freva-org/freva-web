a90136f16401dd6d4dcae0b3888d552744fc1803
kunst <oliver.kunst@met.fu-berlin.de>
2014-03-12 16:33:15 +0100

    Test for django data tables

--- history/urls.py
+++ history/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns('history.views',
     url(r'^$', 'history', name='history'),
+    url(r'^$', 'history2', name='history2'),
     url(r'^(?P<id>\w+)/results/$', 'results', name='results'),
     url(r'^(?P<id>\w+)/jobinfo/$', 'jobinfo', name='jobinfo'),
     url(r'^(?P<id>\w+)/tail-file/$', 'tailFile', name='tailFile'),

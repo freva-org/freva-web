cf473d0425b6a1ac2b67e096b652c8c2a07a06c9
Oliver Kunst <u290038@wwwdev-miklip.dkrz.de>
2014-03-28 18:30:57 +0100

    used django-datatable-view to display the history (speed up by factor 100)

--- history/urls.py
+++ history/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url, patterns
+from django.contrib.auth.decorators import login_required
+from history import views
+
 
 urlpatterns = patterns('history.views',
-    url(r'^$', 'history', name='history'),
-    url(r'^$', 'history2', name='history2'),
+    # url(r'^$', 'history', name='history'),
+    url(r'^$', login_required(views.history2.as_view()), name='history'),
     url(r'^(?P<id>\w+)/results/$', 'results', name='results'),
+    url(r'^(?P<uid>\w+)/show/$', login_required(views.history2.as_view()), name='history'),
     url(r'^(?P<id>\w+)/jobinfo/$', 'jobinfo', name='jobinfo'),
     url(r'^(?P<id>\w+)/tail-file/$', 'tailFile', name='tailFile'),
     url(r'^cancel-slurmjob/$', 'cancelSlurmjob', name='cancelSlurmjob')

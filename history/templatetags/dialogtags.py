bb35d5981ba0935a8168480e38f12232fca5a01e
Oliver Kunst <u290038@wwwdev-miklip.dkrz.de>
2014-04-11 16:52:31 +0200

    share results with other user via email

--- history/templatetags/dialogtags.py
+++ history/templatetags/dialogtags.py
@@ -1,4 +1,9 @@
 from django import template
+from django_evaluation.ldaptools import miklip_user_information
+from django.utils.safestring import mark_safe
+from django_evaluation import settings
+
+import json
 
 register = template.Library()
 
@@ -6,6 +11,28 @@ register = template.Library()
 def cancel_dialog():
     return {}
 
-@register.inclusion_tag('history/templatetags/sendmail_dialog.html')
-def send_mail_dialog():
-    return {}
+@register.inclusion_tag('history/templatetags/sendmail_dialog.html') #, takes_context = True)
+def sendmail_dialog(url, is_guest):
+    return {'url' : url, 'is_guest' : is_guest}
+
+@register.inclusion_tag('history/templatetags/mailfield.html')
+def mailfield(is_guest):
+    info = []
+    user_info = miklip_user_information()
+
+    if is_guest:
+        for uid in settings.EMAIL_RESTRICTIONS:
+            ldap_info = user_info.get_user_info(uid)
+
+            if ldap_info:
+                info.append(ldap_info)
+    else:
+        info = user_info.get_user_info()
+
+    data = []
+
+    for user in info:
+        id = user[0]
+        data.append({'id' : id, 'text' : "%s, %s (%s)" % (user[1], user[2], user[0])})
+    
+    return {'user_data' : mark_safe(json.dumps(data))}

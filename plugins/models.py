b772aa9ac2eca14a4f15865b021b48bca8f8665d
kunst <oliver.kunst@met.fu-berlin.de>
2014-07-10 15:05:18 +0200

    extended version field to 20 varchars. Set default value of version details from 0 to 1

--- plugins/models.py
+++ plugins/models.py
@@ -9,7 +9,7 @@ class Version(models.Model):
     #: Name of the tool
     tool = models.CharField(max_length=50)
     #: Version of the tool
-    version = models.CharField(max_length=10)
+    version = models.CharField(max_length=20)
     #: The tools internal version of a code versioning system 
     internal_version_tool = models.CharField(max_length=40)
     #: The evaluation system's internal version 

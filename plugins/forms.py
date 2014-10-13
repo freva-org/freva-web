90f5fc8a2a2600a48fa1e3025a28d3026fd9dacf
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2014-10-13 15:30:10 +0200

    Sort select options ABC

--- plugins/forms.py
+++ plugins/forms.py
@@ -39,10 +39,12 @@ class PluginFileFieldWidget(Input):
 class PluginSelectFieldWidget(Input):
     def __init__(self,*args,**kwargs):
 	self.options=kwargs.pop('options')
+	import operator
+	self.sorted_options = sorted(self.options.items(), key=operator.itemgetter(1))
 	super(PluginSelectFieldWidget,self).__init__(*args,**kwargs)
 
     def render(self, name, value, attrs=None):
-	return loader.render_to_string('plugins/selectfield.html', {'name':name, 'value':value, 'attrs':attrs, 'options':self.options})
+	return loader.render_to_string('plugins/selectfield.html', {'name':name, 'value':value, 'attrs':attrs, 'options':self.sorted_options})
 
 class SolrFieldWidget(Input):
     def __init__(self, *args, **kwargs):

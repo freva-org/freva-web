73f478e42e4706b5fb4b95400e85629c429f9dc6
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2014-10-16 13:57:19 +0200

    added link to result and started from website to admin

--- history/admin.py
+++ history/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 import logging
 class HistoryAmdin(admin.ModelAdmin):
     
-    list_display = ('id', 'timestamp', 'tool', 'uid_or_str','uid_email','user_name','status_name')
+    list_display = ('id', 'timestamp', 'tool', 'uid_or_str','uid_email','user_name','link_to_model','status_name','started_from_website')
     search_fields = ['tool', 'uid']
     date_hierarchy = 'timestamp'
     search_fields=['uid__username','tool','id']
@@ -12,8 +12,19 @@ class HistoryAmdin(admin.ModelAdmin):
     def uid_email(self, instance):
 	return instance.uid.email
 
+    def link_to_model(self,instance):
+        return '<a href="/history/%s/results/" target="_blank">Show Results</a>' % (instance.id)
+    link_to_model.allow_tags = True
+    
     def user_name(self,instance):
         return instance.uid.first_name + ' ' + instance.uid.last_name
+   
+    def started_from_website(self,instance):
+        if instance.slurm_output == '0':
+            return False
+        else:
+            return True   
+    started_from_website.boolean = True 
 
     def uid_or_str(self,instance):
 	try:

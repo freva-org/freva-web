4cdb32667da167f0d5cff86993dfc7721e97f3b0
Sebastian Illing <sebastian.illing@met.fu-berlin.-de>
2015-08-26 11:34:23 +0200

    django-datatable-view==0.8.3 fixed timestamp problem

--- history/views.py
+++ history/views.py
@@ -52,18 +52,12 @@ class history(XEditableDatatableView):
                      ('User', 'uid'),
                      ('Plugin', 'tool'),
                      ('Caption', 'caption', helpers.make_xeditable()),
-                    # ('Timestamp', 'timestamp', helpers.format_date('%d.%m.%y %H:%M')),
-                     ('Timestamp', None, 'format_date'),
+                     # the timestap year search bug is fixed in django-datatable-view==0.8.3 
+		     ('Timestamp', 'timestamp', helpers.format_date('%d.%m.%y %H:%M')),
                      ('Status', 'get_status_display'),
                      ('Info', 'configuration', 'info_button'),
                     ]
     } 
-
-    def format_date(self, instance, *args, **kwargs):
-        """
-        We use this because we don't want timestamp to be searchable
-        """
-        return instance.timestamp.strftime('%d.%m.%y %H:%M')
         
     def get_queryset(self):
         user = self.kwargs.get('uid', self.request.user)

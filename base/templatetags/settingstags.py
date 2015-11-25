3b2423940ba0a799097a4aa7360de0a5e1280f1e
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2015-11-25 12:11:16 +0100

    shell in a box with settings tag

--- base/templatetags/settingstags.py
+++ base/templatetags/settingstags.py
@@ -27,16 +27,8 @@ def setting_to_list(value):
 
 @register.assignment_tag
 def shell_in_a_box():
-    get_cache('default')
-    shell_status_code = None
-    return False
-    #shell_status_code = cache.get('shell_in_a_box')
-    if not shell_status_code:
-        url = 'https://%s/shell/' % settings.ALLOWED_HOSTS[0]
-        r = requests.get(url)
-        shell_status_code = r.status_code
-        cache.set('shell_in_a_box', shell_status_code, 3600)
-    return True if shell_status_code == 200 else False
+    return getattr(settings, 'SHELL_IN_A_BOX', None)
+
 
 @register.assignment_tag
 def external_group():

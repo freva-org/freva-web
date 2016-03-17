5f2c371306c98fee0eacb1b10305bc2264ced389
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2016-03-17 14:27:19 +0100

    PEP8 for base application

--- base/models.py
+++ base/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 
-def isGuest(self):
-   groups = self.groups.filter(name='Guest')
-   return len(groups) > 0
 
-User.add_to_class('isGuest',isGuest)
+def isGuest(self):
+    groups = self.groups.filter(name='Guest')
+    return len(groups) > 0
+
+# monkeypatch the User class and add 'isGuest' method
+User.add_to_class('isGuest', isGuest)
 
--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,3 @@
-"""urlconf for the base application"""
-
 from django.conf.urls import url, patterns
 
 

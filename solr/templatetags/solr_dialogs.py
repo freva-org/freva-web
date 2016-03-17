ca6eb9e6e3982224038967977ce16fefc843fb32
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2016-03-17 15:00:23 +0100

    PEP8 solr app

--- solr/templatetags/solr_dialogs.py
+++ solr/templatetags/solr_dialogs.py
@@ -1,8 +1,8 @@
 from django import template
-from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+
 @register.inclusion_tag('solr/templatetags/ncdump_dialog.html')
 def ncdump_dialog():
-   return {}
+    return {}

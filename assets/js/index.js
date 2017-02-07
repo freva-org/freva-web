3526c385958789524d0b42e9b47e6d2cec56142b
Sebastian Illing <sebastian.illing@met.fu-berlin.de>
2017-02-07 16:46:17 +0100

    Scroll to top in route change

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,7 +20,7 @@ const history = syncHistoryWithStore(browserHistory, store)
 ReactDOM.render(
     <Provider store={store}>
         { /* Tell the Router to use our enhanced history */ }
-        <Router history={history}>
+        <Router history={history} onUpdate={() => window.scrollTo(0, 0)}>
           <Route path="/" component={App}>
              <Route path="plugins/list/" component={PluginList}/>
              <Route path="plugins/:pluginName/detailnew/" component={PluginDetail}/>

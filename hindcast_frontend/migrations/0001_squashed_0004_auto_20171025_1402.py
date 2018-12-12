a5f339e7e5725d8a13ea1515978fd06608fe0867
Christopher Kadow <christopher.kadow@met.fu-berlin.de>
2018-12-12 16:34:25 +0100

    Squash migrations from hindcast_frontend

--- hindcast_frontend/migrations/0001_squashed_0004_auto_20171025_1402.py
+++ hindcast_frontend/migrations/0001_squashed_0004_auto_20171025_1402.py
@@ -6,6 +6,8 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
+    replaces = [(b'hindcast_frontend', '0001_initial'), (b'hindcast_frontend', '0002_auto_20171024_1533'), (b'hindcast_frontend', '0003_auto_20171025_1358'), (b'hindcast_frontend', '0004_auto_20171025_1402')]
+
     dependencies = [
     ]
 
@@ -14,16 +16,16 @@ class Migration(migrations.Migration):
             name='HindcastEvaluation',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('path', models.CharField(max_length=255)),
                 ('score', models.CharField(max_length=50)),
                 ('eva_time_start', models.IntegerField()),
                 ('eva_time_end', models.IntegerField()),
-                ('eva_type', models.CharField(max_length=10, choices=[(b'map', b'map'), (b'fieldmean', b'fieldmean')])),
                 ('region', models.CharField(max_length=255)),
                 ('time_frequency', models.CharField(max_length=255)),
                 ('variable', models.CharField(max_length=255)),
                 ('reference', models.CharField(max_length=255)),
                 ('hindcast_set', models.CharField(max_length=255)),
+                ('path_fieldmean', models.CharField(max_length=255, null=True, blank=True)),
+                ('path_map', models.CharField(max_length=255, null=True, blank=True)),
             ],
         ),
     ]

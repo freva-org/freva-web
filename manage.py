7e21ce5327ff5ccd030418b59876853b241b4055
m300765 <bergemann@dkrz.com>
2021-05-11 11:26:01 +0200

    Update path

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_evaluation.settings")
     # Add the lib/ directory to the system path
     sys.path.append("lib")
-    sys.path.append('/opt/regiklim-ces/freva/src')
+    sys.path.append('/opt/regiklim-ces-dev/freva/src')
 
     from django.core.management import execute_from_command_line
 

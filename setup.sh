5bb331dc75f42dba7a1022cc88e93667ed3b7912
m300765 <bergemann@dkrz.com>
2021-05-11 11:24:14 +0200

    Install most of the packages from Anaconda

--- setup.sh
+++ setup.sh
@@ -5,19 +5,53 @@ ADMINS=b380001
 
 
 THIS_DIR=$(dirname $(readlink -f $0))
-YOURPYTHON="${THIS_DIR}/venv"
+YOURPYTHON=$(readlink -f "${THIS_DIR}/venv")
 mkdir -p $YOURPYTHON
-shasum=1314b90489f154602fd794accfc90446111514a5a72fe1f71ab83e07de9504a7
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/anaconda.sh
-if [ $(sha256sum /tmp/anaconda.sh| awk '{print $1}') != $shasum ];then
-    >&2 echo 'Checksums do not match'
-    exit 1
-fi
-chmod +x /tmp/anaconda.sh
-/tmp/anaconda.sh -p /tmp/anaconda -b -f -u
-/tmp/anaconda/bin/conda create -c conda-forge -q -p $YOURPYTHON python=$PYTHONVERSION conda pip numpy scipy imagemagick ffmpeg bcrypt mysqlclient pymysql ipyton -y
-let success=$?
-rm -r /tmp/anaconda /tmp/anaconda.sh
-[[ $success -ne 0 ]] && echo "conda create -c conda-forge -q -p $YOURPYTHON python=$PYTHONVERSION -y failed! EXIT" && exit 1
-
+CONDA_PKGS="fabric \
+sphinx \
+nose \
+mock \
+django-nose \
+coverage \
+pep8 \
+pylint \
+django \
+markdown \
+django_compressor \
+bleach \
+python-memcached \
+django-debug-toolbar \
+django-bootstrap3 \
+python-ldap \
+paramiko \
+django-ipware \
+django-model-utils \
+pypdf2 \
+requests \
+pygtail \
+django-webpack-loader \
+djangorestframework \
+conda \
+pip \
+numpy \
+scipy \
+ffmpeg \
+imagemagick \
+mysqlclient \
+pymysql \
+ipython"
+#shasum=1314b90489f154602fd794accfc90446111514a5a72fe1f71ab83e07de9504a7
+#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/anaconda.sh
+#if [ $(sha256sum /tmp/anaconda.sh| awk '{print $1}') != $shasum ];then
+#    >&2 echo 'Checksums do not match'
+#    exit 1
+3fi
+#chmod +x /tmp/anaconda.sh
+#/tmp/anaconda.sh -p /tmp/anaconda -b -f -u
+#/tmp/anaconda/bin/conda create -c conda-forge -q -p $YOURPYTHON python=$PYTHONVERSION $CONDA_PKGS -y
+#let success=$?
+#rm -fr /tmp/anaconda /tmp/anaconda.sh
+#[[ $success -ne 0 ]] && echo "conda create -c conda-forge -q -p $YOURPYTHON python=$PYTHONVERSION $CONDA_PKGS -y failed! EXIT" && exit 1
+${YOURPYTHON}/bin/conda activate $YOURPYTHON
+${YOURPYTHON}/bin/pip install -r requirements.txt
 

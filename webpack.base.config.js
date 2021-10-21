c6507915af087f07fb00561972730351ff42fbb5
k204230 <bergemann@dkrz.de>
2021-10-21 17:35:41 +0200

    Bump webpack version 2 to 4

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -4,14 +4,14 @@ var BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
     context: __dirname,
-
-    entry: ['babel-polyfill', './assets/js/index'],
-
+    mode : 'production',
     output: {
         path: path.resolve('./assets/bundles/'),
-        filename: "[name]-[hash].js"
+        filename: "[name]-[fullhash].js"
     },
-
+    optimization: {
+        moduleIds: 'named',
+    },
     plugins: [],
 
     module: {
@@ -30,7 +30,7 @@ module.exports = {
 
     resolve: {
         modules: ['node_modules', 'bower_components'],
-        extensions: ['', '.js', '.jsx', 'css']
+        extensions: ['', '.js', '.jsx', '.css', '.ts']
     },
 
     externals: [

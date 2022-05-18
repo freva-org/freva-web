0c9dc7e1083fff36b48ce121427342438c487c03
Andrej Fast <andrej.fast@dkrz.de>
2022-05-18 13:55:38 +0200

    removed the find_owner functionality

--- plugins/forms.py
+++ plugins/forms.py
@@ -6,7 +6,7 @@ from django.template import loader
 import evaluation_system.api.parameters as parameters
 from evaluation_system.api import plugin_manager as pm
 from pathlib import Path
-from plugins.utils import ssh_call, get_scheduler_hosts, find_owner
+from plugins.utils import ssh_call
 from evaluation_system.misc.utils import PrintableList
 from django.conf import settings
 
@@ -215,12 +215,6 @@ class PluginForm(forms.Form):
                     widget=PluginFileFieldWidget(file_extension=param.file_extension),
                 )
             elif isinstance(param, parameters.Directory):
-                if self.initial.get(key, None):
-                    rel_path = Path(self.initial[key]).relative_to(self._workdir)
-                    add_warn = '<br><span style="color:red">Warning! This is not your directory.</span>'
-                    if find_owner(self.initial[key]) == uid or rel_path.parts[0] == uid:
-                        add_warn = ""
-                    help_str += add_warn
                 self.fields[key] = forms.CharField(
                     required=required, help_text=help_str
                 )
--- plugins/utils.py
+++ plugins/utils.py
@@ -5,19 +5,6 @@ from django.http import Http404
 from django.conf import settings
 import paramiko
 import logging
-from os import stat, path
-from pwd import getpwuid
-
-
-def find_owner(filename):
-    """
-    Return the owner of a file or directory recursively
-    """
-    if path.exists(filename):
-        return getpwuid(stat(filename).st_uid).pw_name
-    if filename[0] != "/":
-        return True
-    return find_owner("/".join(filename.split("/")[:-1]))
 
 
 def get_scheduler_hosts(user):

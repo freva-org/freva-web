8e53c3b9cd64e7ed178741fe85099dc47689c572
k204230 <bergemann@dkrz.de>
2022-07-11 12:19:35 +0200

    Re add lost bundels dir

--- django_evaluation/settings/local.py
+++ django_evaluation/settings/local.py
@@ -194,10 +194,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     os.path.join(PROJECT_ROOT, "static_root"),
+    ("assets/bundles", os.path.join(PROJECT_ROOT, "assets", "bundles")),
 )
-# if not DEV:
-# config.reloadConfiguration()
-# STATICFILES_DIRS += (("preview", config.get("preview_path")),)
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (

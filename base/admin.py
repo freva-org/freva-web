58541c7a0efade3479e14f4e355f709cf7ddfd46
Andrej Fast <andrej.fast@dkrz.de>
2022-11-07 09:56:03 +0100

    registered UiMessages to Admin-Panel

--- base/admin.py
+++ base/admin.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 from django.contrib.auth.models import Permission
 
+from .models import UIMessages
+
+admin.site.register(UIMessages)
 admin.site.unregister(User)
 
 

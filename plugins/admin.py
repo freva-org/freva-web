f262ee19e27fecb4387e668120b6ae456fcd27ed
Andrej Fast <andrej.fast@dkrz.de>
2023-03-09 10:17:14 +0100

    deleted superfluous toolpullrequest-thingie from the admin-view

--- plugins/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from plugins.models import ToolPullRequest
-
-
-class ToolPullRequestAdmin(admin.ModelAdmin):
-    list_display = ("id", "created", "tool", "tagged_version", "user", "status")
-
-
-admin.site.register(ToolPullRequest, ToolPullRequestAdmin)

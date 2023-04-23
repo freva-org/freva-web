c2c6f3474e7b8d29d265967c4af572a6c98747ea
antarcticrainforest <martin.bergemann@posteo.org>
2023-04-23 06:36:20 +0200

    Add smoke tests.

--- tests/test_npm.py
+++ tests/test_npm.py
@@ -13,17 +13,17 @@ def test_npm_install(eval_config):
     assert res.returncode == 0
 
 
-# def test_npm_build_prod(eval_config):
-#    """Test the npm build production."""
-#    cmd = "npm run build-production"
-#    res = run(shlex.split(cmd), cwd=CWD, stderr=PIPE, stdout=PIPE, check=False)
-#    print(res.stderr.decode())
-#    assert res.returncode == 0
+def test_npm_build_prod(eval_config):
+    """Test the npm build production."""
+    cmd = "npm run build-production"
+    res = run(shlex.split(cmd), cwd=CWD, stderr=PIPE, stdout=PIPE, check=False)
+    print(res.stderr.decode())
+    assert res.returncode == 0
 
 
-# def test_npm_build(eval_config):
-#    """Test the npm build."""
-#    cmd = "npm run build"
-#    res = run(shlex.split(cmd), cwd=CWD, stderr=PIPE, stdout=PIPE, check=False)
-#    print(res.stderr.decode())
-#    assert res.returncode == 0
+def test_npm_build(eval_config):
+    """Test the npm build."""
+    cmd = "npm run build"
+    res = run(shlex.split(cmd), cwd=CWD, stderr=PIPE, stdout=PIPE, check=False)
+    print(res.stderr.decode())
+    assert res.returncode == 0

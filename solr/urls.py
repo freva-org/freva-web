a1bf5ebc38e3969e57fd468644ac54fbfeabb0d3
Andrej Fast <andrej.fast@dkrz.de>
2023-08-04 15:15:10 +0200

    started with intake and plugin-setups, still WIP

--- solr/urls.py
+++ solr/urls.py
@@ -14,9 +14,19 @@ urlpatterns = [
     url(r"^solr-search/$", solr.views.solr_search, name="solr_search"),
     url(r"^data-browser/$", solr.views.databrowser, name="data_browser"),
     path(
-        "search/<str:core>/<str:unique_key>/",
+        "search/<str:flavour>/<str:unique_key>/",
         solr.views.search,
         name="api_search",
+    ),
+    path(
+        "facet/<str:flavour>/",
+        solr.views.get_single_facet,
+        name="api_facet",
+    ),
+    path(
+        "intake_catalogue/<str:flavour>/<str:unique_key>/",
+        solr.views.get_intake_catalogue,
+        name="api_intake",
     ),
     url(
         r"^overview/$",
--- solr/views.py
+++ solr/views.py
@@ -132,15 +132,48 @@ def solr_search(request):
             content_type="application/json",
         )
 
-def search(request, core, unique_key):
+
+def search(request, flavour, unique_key):
     return reverse_proxy(
-        request, f"http://localhost:7777/metadata_search/{core}/{unique_key}"
+        request, f"http://localhost:7777/metadata_search/{flavour}/{unique_key}"
     )
+
+
+def get_single_facet(request, flavour):
+    query_params = request.GET.copy()
+    facets = query_params.getlist("facets")
+    if len(facets) != 1:
+        return JsonResponse(
+            {"error": "Only one queried facet at a time allowed"}, status=400
+        )
+    facet = facets[0]
+    # filtered_facets = query_params.get(facet)
+    # if filtered_facets:
+    # del query_params[facet]
+
+    query_params["batch_size"] = 1
+    request.GET = query_params
+
+    response = reverse_proxy(
+        request, f"http://localhost:7777/metadata_search/{flavour}/file"
+    )
+    print(response.content)
+    data = json.loads(response.content)["facets"]
+    if facet in data:
+        return JsonResponse(
+            {"facets": {facet: data[facet]}}, status=response.status_code
+        )
+    return JsonResponse({}, status=response.status_code)
 
 
 def get_search_overview(request):
     return reverse_proxy(request, "http://localhost:7777/overview")
 
+
+def get_intake_catalogue(request, flavour, unique_key):
+    return reverse_proxy(
+        request, f"http://localhost:7777/intake_catalouge/{flavour}/{unique_key}"
+    )
 
 def reverse_proxy(request, path):
     api_url = path

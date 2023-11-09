c1e4b5c087836cf88e5cace48ddfb333d816ab8d
Andrej Fast <andrej.fast@dkrz.de>
2023-11-09 08:57:14 +0100

    forgot an import..

--- plugins/views.py
+++ plugins/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.views.decorators.debug import (

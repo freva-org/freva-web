89890a2278d36230b0b985f366bee0da5662e16d
Bianca Wentzel <wentzel@dkrz.de>
2024-09-11 16:29:55 +0200

    added example request on climate data

--- assets/js/Containers/FrevaGPT/constants.js
+++ assets/js/Containers/FrevaGPT/constants.js
@@ -1,14 +1,14 @@
 export const recentRequests = [
     {
-        title: "Use code_interpreter to count from 0 to 10",
-        thread: "Md1TYf5OGwcVlVNqx7C4UDincrFuH3DV"
-    },
-    {
-        title: "plot sine curve",
-        thread: "VRW6JtrX7XvkJ07FEky9HAs24ZgVH7dq"
-    },
-    {
         title: "what is wind",
         thread: "jsv56xRwPO53XRRPTBD65iYIxf18MOvH",
+    },
+    {
+        title: "Make an annual mean temperature global map plot for the year 2023",
+        thread: "OjsErODeP7cyYbLx3tqLg7SXBNkcmums"
+    },
+    {
+        title: "Make a zonal mean plot of precipitation for the average between 1961 and 1990 as well as 1991 and 2020",
+        thread: "zGDb04pJcBfL3HrkK1yKbrG0vMaWKd3w"
     }
 ];

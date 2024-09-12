b654c8f35ebd647cbedf926645d0e6e1e9463a85
Bianca Wentzel <wentzel@dkrz.de>
2024-09-12 09:25:33 +0200

    Formatted Code String

--- assets/js/Containers/FrevaGPT/CodeBlock.js
+++ assets/js/Containers/FrevaGPT/CodeBlock.js
@@ -4,8 +4,13 @@ import { Accordion } from 'react-bootstrap';
 import PropTypes from "prop-types";
 
 function formatCode(codeString) {
-    const seperatedCode = codeString[0].split("\\n");
-    return seperatedCode;
+    // remove everyting before json codeblock
+    let newString = codeString[0].slice(codeString[0].indexOf(":") + 2);
+    newString = newString.slice(0, -2);
+
+    const codeSnippets = newString.split("\\n");
+
+    return codeSnippets;
 }
 
 function CodeBlock(props) {

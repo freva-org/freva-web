024a42208dc9240e3316c8e3a504c59a61ae5ca7
Bianca Wentzel <wentzel@dkrz.de>
2024-09-26 12:53:26 +0200

    update requests, added freva requests

--- assets/js/Containers/FrevaGPT/constants.js
+++ assets/js/Containers/FrevaGPT/constants.js
@@ -2,33 +2,47 @@ export const botRequests = {
     general: [
         {
             title: "Name the most important parameters in meteorology",
-            thread: "09OYj1UhBIyzakMAEHDbyXGu0d1UavZZ",
+            thread: "iEgc4k8vsrpQYbvHT3u0tzGmrZHBjdik",
         },
         {
             title: "What is ENSO",
-            thread: "OvWhPEkNAeVDjxANpJCFLRvjPJnq01mt",
+            thread: "dVzyCJGCqcqIsjdap1yMciHrbgcKkUod",
         },
         {
             title: "What is the difference between RCP scenarios and SSp scenarios",
-            thread: "qIZyDxfUOdTecCJ5kgKir6MDY7olvEMw"
+            thread: "RPLDnwvPUBgSrh6x2iHJcnVgNf1Bbx6H"
         },
         {
             title: "Name the most common climate models",
-            thread: "VcQc3ZuWMDWFluLLQd2YzaNIqYoSxi6P"
+            thread: "dQ6Jck4eo3REt6Q1pJasjrJRdT4t3yOq"
         }
     ],
     evaluation: [
         {
             title: "Make an annual mean temperature global map plot for the year 2023",
-            thread: "J8W0gvvDl7U1A7OkHQ8UhcUYa8810UhL",
+            thread: "QkUAN3UAJFYhRQXg5jRqisoeOZ1uwPYK",
         },
         {
             title: "Make a zonal mean plot of precipitation for the average between 1961 and 1990 as well as 1991 and 2020",
-            thread: "yvgkWrPQlZPLztLTzvU3BE6v7yENvjIU",
+            thread: "NjVqnVTxzYuWl8tcgWiREaU525NE875H",
         },
         {
             title: "Find the year with the highest local wind speed, make a map plot of wind and mark the grid box with an X",
-            thread: "2YNjKiekgLAGKHL5WrHa9JXe3Gknfxax",
+            thread: "TAyYnt3amopwJgJOh0Zim2t80KwRED44",
         }
     ],
+    freva: [
+        {
+            title: "Use the freva databrowser to extract the number of datasets for the project cmip5 and the variable cli",
+            thread: "faWvwiqUGwC6cJq9OL4bSmWytkLxyYkQ"
+        },
+        {
+            title: "Which plugins are integrated into freva?",
+            thread: "66Rtx8qGrAHUza5DRjUBCkgblBGnE7yb"
+        },
+        {
+            title: "What is freva?",
+            thread: "Pp6xO7lLI4j9LGx34AIGcPGbgxLFq5jE"
+        }
+    ]
 };

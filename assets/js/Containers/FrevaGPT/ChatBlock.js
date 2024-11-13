77c5c092d68a7dd0f04141c9732127aaad0d2961
Bianca Wentzel <wentzel@dkrz.de>
2024-11-13 09:16:32 +0100

    Fixed linting

--- assets/js/Containers/FrevaGPT/ChatBlock.js
+++ assets/js/Containers/FrevaGPT/ChatBlock.js
@@ -16,6 +16,89 @@ class ChatBlock extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.renderImage = this.renderImage.bind(this);
+        this.renderCode = this.renderCode.bind(this);
+        this.renderUser = this.renderUser.bind(this);
+        this.renderError = this.renderError.bind(this);
+        this.renderDefault = this.renderDefault.bind(this);
+    }
+
+    
+
+    renderImage(element) {
+      return (
+        <Col key={element.content} md={{span: 10, offset: 0}}>
+          <img className="w-100" src={`data:image/jpeg;base64,${element.content}`} />
+        </Col>
+      );
+    }
+
+    renderCode(element) {
+      if (isEmpty(element.content[0])) return null;
+      else return(
+        <Col md={{span:10, offset: 0}} key={element.content}>
+          <CodeBlock title={element.variant} code={element.content}/>
+        </Col>
+      );
+    }
+
+    renderUser(element) {
+      return (
+        <Col md={{span: 10, offset: 2}} key={element.content}>
+          <Card className="shadow-sm card-body border-0 border-bottom mb-3 bg-info">
+              {element.content}
+          </Card>
+        </Col>
+      );
+    }
+
+    renderError(element) {
+      return(
+        <Col md={{span: 10, offset: 0}} key={element.content}>
+          <Card className="shadow-sm card-body border-0 border-bottom mb-3 bg-danger">
+            <span className="fw-bold">{element.variant}</span>
+            <Markdown>{replaceLinebreaks(element.content)}</Markdown>
+          </Card>
+        </Col>
+      );
+    }
+
+    renderDefault(element) {
+      return (
+        <Col md={{span: 10, offset: 0}} key={element.content}>
+          <Card className="shadow-sm card-body border-0 border-bottom mb-3 bg-light">
+            <Markdown>{replaceLinebreaks(element.content)}</Markdown>
+          </Card>
+        </Col>
+      );  
+    }
+
+    renderChatComponents(element) {
+      switch(element.variant) {
+        case "ServerHint":
+        case "StreamEnd":
+          return null
+        case "Image":
+          return this.renderImage(element);
+
+        case "Code":
+        case "CodeOutput":
+          return  this.renderCode(element);
+
+        case "User":
+          return this.renderUser(element);
+
+        case "ServerError":
+        case "OpenAIError":
+        case "CodeError":
+        case "FrontendError":
+        case "UserStop":
+          return this.renderError(element);
+
+        default:
+          return this.renderDefault(element);
+      }
     }
 
     render() {
@@ -24,58 +107,7 @@ class ChatBlock extends React.Component {
 
         return(
             <Col>
-               {conversation.map((element) => {
-                if (element.variant !== "ServerHint" && element.variant !== "StreamEnd") {
-                  switch(element.variant){
-                    case "Image":
-                      return (
-                        <Col key={element.content} md={{span: 10, offset: 0}}>
-                          <img className="w-100" src={`data:image/jpeg;base64,${element.content}`} />
-                        </Col>
-                      );
-  
-                    case "Code":
-                    case "CodeOutput":
-                      if (isEmpty(element.content[0])) return null;
-                      else return(
-                        <Col md={{span:10, offset: 0}} key={element.content}>
-                          <CodeBlock title={element.variant} code={element.content}/>
-                        </Col>
-                      );
-  
-                    case "User":
-                      return (
-                        <Col md={{span: 10, offset: 2}} key={element.content}>
-                          <Card className="shadow-sm card-body border-0 border-bottom mb-3 bg-info">
-                              {element.content}
-                          </Card>
-                        </Col>
-                      );
-                    case "ServerError":
-                    case "OpenAIError":
-                    case "CodeError":
-                    case "FrontendError":
-                    case "UserStop":
-                      return(
-                        <Col md={{span: 10, offset: 0}} key={element.content}>
-                          <Card className="shadow-sm card-body border-0 border-bottom mb-3 bg-danger">
-                            <span className="fw-bold">{element.variant}</span>
-                            <Markdown>{replaceLinebreaks(element.content)}</Markdown>
-                          </Card>
-                        </Col>
-                      );
-                    default:
-                      return (
-                        <Col md={{span: 10, offset: 0}} key={element.content}>
-                          <Card className="shadow-sm card-body border-0 border-bottom mb-3 bg-light">
-                            <Markdown>{replaceLinebreaks(element.content)}</Markdown>
-                          </Card>
-                        </Col>
-                      );  
-                  }
-                }
-              }
-              )}
+               {conversation.map((element) => {return this.renderChatComponents(element)})}
             </Col>
         )
     }

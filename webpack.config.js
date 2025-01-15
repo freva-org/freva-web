617f0109591134228cc23933e5fa738c77c3b00c
Bianca Wentzel <wentzel@dkrz.de>
2025-01-15 15:39:32 +0100

    Removed unused dependencies from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,6 @@ const { merge } = require("webpack-merge");
 const baseConfig = require("./webpack/webpack.base.config");
 const devConfig = require("./webpack/webpack.dev.config");
 const prodConfig = require("./webpack/webpack.prod.config");
-const webpack = require('webpack');
-
-const Dotenv = require("dotenv-webpack");
 
 let config;
 if (["build", "dev"].includes(process.env.npm_lifecycle_event)) {

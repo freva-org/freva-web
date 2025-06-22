a902b02d1b094f8d56dc2f6719e0bc71932b5338
Mo <hadizadeh@dkrz.de>
2025-06-22 11:45:22 +0200

    since the guest is not able to run plugin, let them plug their plugin if they have access to the HPC and see how it works

--- assets/js/Containers/App/index.js
+++ assets/js/Containers/App/index.js
@@ -37,7 +37,6 @@ App.propTypes = {
     username: PropTypes.string,
     email: PropTypes.string,
     first_name: PropTypes.string,
-    isGuest: PropTypes.bool,
     scratch: PropTypes.string,
   }),
   error: PropTypes.string,
--- base/serializers.py
+++ base/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    isGuest = serializers.SerializerMethodField()
     scratch = serializers.SerializerMethodField()
 
     class Meta:
@@ -14,15 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
             "email",
             "first_name",
             "scratch",
-            "isGuest",
         )
-
-    def get_isGuest(self, instance):
-        """Check if the user is a guest."""
-        request = self.context.get("request")
-        if request.session.get("system_user_valid", False):
-            return False
-        return True
 
     def get_scratch(self, instance):
         """Get user scratch directory."""

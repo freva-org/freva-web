e99062b1fda960aa3f9a8ec81bee0b71a3c99069
BiancaWentzel <courtney_boda@gmx.de>
2025-08-06 16:18:36 +0200

    Refactored CodeBlock component to functional component

--- assets/js/Containers/FrevaGPT/components/CodeBlock.js
+++ assets/js/Containers/FrevaGPT/components/CodeBlock.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Collapse, Button } from "react-bootstrap";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 
@@ -12,89 +12,73 @@ import PropTypes from "prop-types";
 
 import { formatCode } from "../utils";
 
-class CodeBlock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.toggleShowCode = this.toggleShowCode.bind(this);
-    this.extractElements = this.extractElements.bind(this);
+function CodeBlock({ content }) {
+  const [showCode, setShowCode] = useState(true);
 
-    this.state = {
-      showCode: true,
-    };
+  function toggleShowCode() {
+    setShowCode(!showCode);
   }
 
-  toggleShowCode(status) {
-    this.setState({ showCode: !status });
-  }
-
-  extractElements(content, variant) {
+  function extractElements(content, variant) {
     return content.filter((elem) => elem.variant === variant);
   }
 
-  render() {
-    return (
-      <Card className="shadow-sm card-body border-0 border-bottom mb-3 bg-light">
-        <Button
-          variant="link"
-          className="m-0 p-0 d-inline-flex text-decoration-none"
-          onClick={() => {
-            this.toggleShowCode(this.state.showCode);
-          }}
-        >
-          <span style={{ fontWeight: "bold" }} className="color">
-            Analyzed
-          </span>
-          <span>
-            {this.state.showCode ? (
-              <FaAngleUp className="color" />
-            ) : (
-              <FaAngleDown className="color" />
-            )}
-          </span>
-        </Button>
+  return (
+    <Card className="shadow-sm card-body border-0 border-bottom mb-3 bg-light">
+      <Button
+        variant="link"
+        className="m-0 p-0 d-inline-flex text-decoration-none"
+        onClick={() => {
+          toggleShowCode();
+        }}
+      >
+        <span style={{ fontWeight: "bold" }} className="color">
+          Analyzed
+        </span>
+        <span>
+          {showCode ? (
+            <FaAngleUp className="color" />
+          ) : (
+            <FaAngleDown className="color" />
+          )}
+        </span>
+      </Button>
 
-        <Collapse in={this.state.showCode} className="mt-2">
-          <Card className="shadow-sm">
-            <Card.Header style={{ backgroundColor: "#eee" }}>
-              python
-            </Card.Header>
+      <Collapse in={showCode} className="mt-2">
+        <Card className="shadow-sm">
+          <Card.Header style={{ backgroundColor: "#eee" }}>python</Card.Header>
 
-            {this.extractElements(this.props.content, "Code").map(
-              (codeElement) => {
-                return (
-                  <Card.Body
-                    className="p-0 m-0 border-bottom"
-                    key={`${codeElement.content[1]}-code`}
-                    style={{ backgroundColor: "#fafafa" }}
-                  >
-                    <SyntaxHighlighter language="python" style={oneLight}>
-                      {formatCode("Code", codeElement.content[0])}
-                    </SyntaxHighlighter>
-                  </Card.Body>
-                );
-              }
-            )}
+          {extractElements(content, "Code").map((codeElement) => {
+            return (
+              <Card.Body
+                className="p-0 m-0 border-bottom"
+                key={`${codeElement.content[1]}-code`}
+                style={{ backgroundColor: "#fafafa" }}
+              >
+                <SyntaxHighlighter language="python" style={oneLight}>
+                  {formatCode("Code", codeElement.content[0])}
+                </SyntaxHighlighter>
+              </Card.Body>
+            );
+          })}
 
-            {this.extractElements(this.props.content, "CodeOutput").map(
-              (codeElement) => {
-                return (
-                  <Card.Footer
-                    className="p-0 m-0"
-                    key={`${codeElement.content[1]}-codeoutput`}
-                    style={{ backgroundColor: "#263238", fontSize: "0.72em" }}
-                  >
-                    <SyntaxHighlighter language="python" style={materialDark}>
-                      {formatCode("CodeOutput", codeElement.content[0])}
-                    </SyntaxHighlighter>
-                  </Card.Footer>
-                );
-              }
-            )}
-          </Card>
-        </Collapse>
-      </Card>
-    );
-  }
+          {extractElements(content, "CodeOutput").map((codeElement) => {
+            return (
+              <Card.Footer
+                className="p-0 m-0"
+                key={`${codeElement.content[1]}-codeoutput`}
+                style={{ backgroundColor: "#263238", fontSize: "0.72em" }}
+              >
+                <SyntaxHighlighter language="python" style={materialDark}>
+                  {formatCode("CodeOutput", codeElement.content[0])}
+                </SyntaxHighlighter>
+              </Card.Footer>
+            );
+          })}
+        </Card>
+      </Collapse>
+    </Card>
+  );
 }
 
 CodeBlock.propTypes = {

6f291c1f1ed260227a1a15c6d82e5455d6e6a763
BiancaWentzel <courtney_boda@gmx.de>
2025-09-04 09:55:44 +0200

    Added functionality to change thread topic

--- assets/js/Containers/FrevaGPT/components/SidePanel/ThreadLink.js
+++ assets/js/Containers/FrevaGPT/components/SidePanel/ThreadLink.js
@@ -3,18 +3,25 @@ import PropTypes from "prop-types";
 
 import { browserHistory } from "react-router";
 
+import { isEmpty } from "lodash";
+
 import {
   OverlayTrigger,
   Tooltip,
   Badge,
   ListGroup,
   Popover,
+  Modal,
+  Button,
+  FormControl,
 } from "react-bootstrap";
 
 import { FaEllipsisH, FaEdit } from "react-icons/fa";
 
 function ThreadLink({ element }) {
   const [showOptions, setShowOptions] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [topic, setTopic] = useState(element.topic);
 
   function changeToThread(thread) {
     browserHistory.push({
@@ -23,15 +30,18 @@ function ThreadLink({ element }) {
     });
   }
 
-  function showThreadOptions() {
-    setShowOptions(true);
+  function handleTopicInput(e) {
+    setTopic(e.target.value);
   }
 
-  function hideThreadOptions() {
-    setShowOptions(false);
+  function renameThread() {
+    // TODO: add backend call to change topic of thread
+    if (!isEmpty(topic) && topic !== element.topic) {
+      //request change to backend
+      //change in frontend -> re-request or manual change?
+    }
+    setShowModal(false);
   }
-
-  function renameThread() {}
 
   const popover = (
     <Popover id="popover-basic">
@@ -39,7 +49,7 @@ function ThreadLink({ element }) {
         <ListGroup.Item
           action
           className="d-flex align-items-center"
-          onClick={renameThread}
+          onClick={() => setShowModal(true)}
         >
           <FaEdit style={{ marginRight: "5px" }} />
           Rename
@@ -49,31 +59,61 @@ function ThreadLink({ element }) {
   );
 
   return (
-    <div
-      className="mb-2 color d-flex justify-content-between"
-      onMouseEnter={showThreadOptions}
-      onMouseLeave={hideThreadOptions}
-    >
-      <OverlayTrigger
-        key={`${element.thread_id}-tooltip`}
-        overlay={<Tooltip>{element.topic}</Tooltip>}
+    <>
+      <div
+        className="mb-2 color d-flex justify-content-between"
+        onMouseEnter={() => setShowOptions(true)}
+        onMouseLeave={() => setShowOptions(false)}
       >
-        <a
-          href=""
-          className="text-truncate"
-          onClick={() => changeToThread(element.thread_id)}
+        <OverlayTrigger
+          key={`${element.thread_id}-tooltip`}
+          overlay={<Tooltip>{element.topic}</Tooltip>}
         >
-          {element.topic}
-        </a>
-      </OverlayTrigger>
-      {showOptions ? (
-        <OverlayTrigger trigger="click" placement="right" overlay={popover}>
-          <Badge bg="secondary" as="button">
-            <FaEllipsisH />
-          </Badge>
+          <a
+            href=""
+            className="text-truncate"
+            onClick={() => changeToThread(element.thread_id)}
+          >
+            {element.topic}
+          </a>
         </OverlayTrigger>
-      ) : null}
-    </div>
+        {showOptions ? (
+          <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+            <Badge bg="secondary" as="button">
+              <FaEllipsisH />
+            </Badge>
+          </OverlayTrigger>
+        ) : null}
+      </div>
+
+      <Modal
+        size="s"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={showModal}
+        onHide={() => {
+          setShowModal(false);
+        }}
+      >
+        <Modal.Header closeButton>Rename chat</Modal.Header>
+        <Modal.Body>
+          <FormControl
+            as="textarea"
+            value={topic}
+            onChange={handleTopicInput}
+            placeholder="Set a title"
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Cancel
+          </Button>
+          <Button variant="info" onClick={renameThread}>
+            Rename
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 

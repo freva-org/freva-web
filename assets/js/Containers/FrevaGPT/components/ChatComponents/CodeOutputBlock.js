97ec3177b3faf6cd261446a4a99c4150fa1a8833
BiancaWentzel <courtney_boda@gmx.de>
2026-08-07 13:43:39 +0200

    Adapted handling of additional code output

--- assets/js/Containers/FrevaGPT/components/ChatComponents/CodeOutputBlock.js
+++ assets/js/Containers/FrevaGPT/components/ChatComponents/CodeOutputBlock.js
@@ -7,9 +7,11 @@ import PropTypes from "prop-types";
 import hljs from "highlight.js";
 
 import "highlight.js/styles/atom-one-dark.css";
-import { formatCode } from "../../utils";
+import { extractOutput } from "../../utils";
 
 function CodeOutputBlock({ content }) {
+  const output = extractOutput(content);
+
   useEffect(() => {
     // hilights all code elements
     document.querySelectorAll(".codeoutputblock code").forEach((block) => {
@@ -20,15 +22,11 @@ function CodeOutputBlock({ content }) {
   }, []);
 
   function renderCodeOutput(content) {
-    if (!isEmpty(content)) {
+    if (!isEmpty(output)) {
       return (
         <Card.Footer className="p-0 m-0" key={`${content.id}-codeoutput`}>
           <pre className="codeoutputblock m-0">
-            <code className="bot-code-output">
-              {typeof content.content === String
-                ? formatCode("CodeOutput", content.content)
-                : JSON.stringify(content.content)}
-            </code>
+            <code className="bot-code-output">{output}</code>
           </pre>
         </Card.Footer>
       );
